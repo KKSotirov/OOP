e3719ed1a65eec9dfc5ea89139dd9668d7610a2f
KKSotirov <sotirov.k73@gmail.com>
2026-07-17 02:16:35 +0300

    Start of YGO C++ game

---
+++ Card Duel & Combo Engine.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <cstring>
+#include <utility>
+
+class Card
+{
+private:
+    char *name;
+    char *effect;
+
+    void free()
+    {
+        delete[] name;
+        delete[] effect;
+    }
+
+protected:
+    void swap(Card &other)
+    {
+        std::swap(this->name, other.name);
+        std::swap(this->effect, other.effect);
+    }
+
+    void setterHelper(char *dest, const char *src)
+    {
+        char *temp = src ? new char[strlen(src) + 1] : nullptr;
+        if (src != nullptr)
+            strcpy(temp, src);
+        delete[] dest;
+        dest = temp;
+    }
+
+public:
+    // Def constructor
+    Card() : name(nullptr) {}
+    // Par constructor
+    Card(const char *newName, const char *newEffect)
+    {
+        setterHelper(this->name, newName);
+        setterHelper(this->effect, newEffect);
+    }
+    // Operator = using copy and swap
+    Card &operator=(Card other)
+    {
+        this->swap(other);
+        return *this;
+    }
+    // Virtual destructor
+    virtual ~Card()
+    {
+        free();
+    }
+
+    // GETTERS + SETTERS
+    void setName(const char *newName)
+    {
+        setterHelper(this->name, newName);
+    }
+    char *getName() const
+    {
+        return this->name;
+    }
+    void setEffect(const char *newEffect)
+    {
+        setterHelper(this->effect, newEffect);
+    }
+    char *getEffect() const
+    {
+        return this->effect;
+    }
+
+    // Virtual functions
+    virtual void play() const = 0;
+    virtual Card *clone() const = 0;
+};
+
+class MonsterCard : virtual public Card
+{
+private:
+    int attack;
+    int defense;
+
+public:
+    // Default constructor
+    MonsterCard() : Card(), attack(0), defense(0) {}
+    // Par constructor
+    MonsterCard(const int newAttack, const int newDefense, const char *cardName, const char *cardEffect) : Card(cardName, cardEffect), attack(newAttack), defense(newDefense) {}
+    // Copy constructor
+    MonsterCard(const MonsterCard &other)
+    {
+        this->setName(other.getName());
+        this->attack = other.attack;
+        this->defense = other.defense;
+    }
+    // NO dyn mem ~~> no ro3
+    int getAttack() const
+    {
+        return this->attack;
+    }
+    void setAttack(const int newAttack)
+    {
+        this->attack = newAttack;
+    }
+    int getDefense() const
+    {
+        return this->defense;
+    }
+    void setAefense(const int newdefense)
+    {
+        this->defense = newdefense;
+    }
+
+    // Virtual functions
+    virtual MonsterCard *clone() const override
+    {
+        return new MonsterCard(*this);
+    }
+    virtual void play() const override
+    {
+        std::cout << "[Card Name] ~~> " << this->getName()
+                  << ", with stats : {ATK = " << this->attack
+                  << "}, {DEFF = " << this->defense << "} !" << std::endl;
+    }
+};
--- Summer vibecoding/tutorial.cpp
+++ Summer vibecoding/tutorial.cpp
@@ -9,9 +9,26 @@ private:
     int bonusHealth;
     int bonusAttack;
 
-protected:
-    void copyObj(const char *str)
+    void free()
     {
+        delete[] name;
+    }
+
+protected:
+    void setterHelper(char *dest, const char *src)
+    {
+        char *temp = src ? new char[strlen(src) + 1] : nullptr;
+        if (src != nullptr)
+            strcpy(temp, src);
+        delete[] dest;
+        dest = temp;
+    }
+
+    void swap(Item &other)
+    {
+        std::swap(this->bonusAttack, other.bonusAttack);
+        std::swap(this->bonusHealth, other.bonusHealth);
+        std::swap(this->name, other.name);
     }
 
 public:
@@ -20,5 +37,53 @@ public:
     // parametrised constructor
     Item(const char *str, const int bHealth, const int bAttack) : bonusHealth(bHealth), bonusAttack(bAttack)
     {
+        setterHelper(this->name, str);
     }
+    // Operator =
+    Item &operator=(Item other)
+    {
+        this->swap(other);
+        return *this;
+    }
+    // virtual destructor
+    virtual ~Item()
+    {
+        free();
+    }
+
+    // GETTERS + SETTERS
+    int getBonusAttack() const
+    {
+        return this->bonusAttack;
+    }
+    void setBonusAttack(const int newBonusAtack)
+    {
+        this->bonusAttack = newBonusAtack;
+    }
+    int getBonusHealth() const
+    {
+        return this->bonusHealth;
+    }
+    void setBonusHealth(const int newBonusHealth)
+    {
+        this->bonusHealth = newBonusHealth;
+    }
+    void setName(const char *newName)
+    {
+        setterHelper(this->name, newName);
+    }
+    const char *getName()
+    {
+        return this->name;
+    }
+};
+
+class Champion : public Item
+{
+private:
+    int baseHealth;
+    int baseAD;
+    Item **inventory;
+    int count;
+    int capacity;
 };
