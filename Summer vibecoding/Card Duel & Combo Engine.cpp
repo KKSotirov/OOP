7d2d7086719afbcb4f5c4d7661b6fadeda59ae33
KKSotirov <sotirov.k73@gmail.com>
2026-07-18 18:12:36 +0300

    All card types + diamon problem (Pendulums)

--- Summer vibecoding/Card Duel & Combo Engine.cpp
+++ Summer vibecoding/Card Duel & Combo Engine.cpp
@@ -12,7 +12,26 @@ enum class SpellType
     POLYMERIZATION,
     Normal
 };
-
+void printSpellType(SpellType spellType)
+{
+    switch (spellType)
+    {
+    case SpellType::QuickPlay:
+        std::cout << "QuickPlay Spell" << std::endl;
+    case SpellType::Continuous:
+        std::cout << "Continuous Spell" << std::endl;
+    case SpellType::Equip:
+        std::cout << "Equip Spell" << std::endl;
+    case SpellType::Field:
+        std::cout << "Field Spell" << std::endl;
+    case SpellType::Ritual:
+        std::cout << "Ritual Spell" << std::endl;
+    case SpellType::POLYMERIZATION:
+        std::cout << "POLYMERIZATION" << std::endl;
+    default:
+        std::cout << "Normal Spell" << std::endl;
+    }
+}
 enum class TrapType
 {
     Counter,
@@ -20,6 +39,18 @@ enum class TrapType
     Normal
 };
 
+void printTrapType(TrapType trapType)
+{
+    switch (trapType)
+    {
+    case TrapType::Counter:
+        std::cout << "Counter Trap" << std::endl;
+    case TrapType::Continuous:
+        std::cout << "Continuous Trap" << std::endl;
+    default:
+        std::cout << "Normal trap" << std::endl;
+    }
+}
 class Card
 {
 private:
@@ -165,7 +196,9 @@ public:
     virtual void play() const override
     {
         std::cout << "[SPELL CARD] ~~> {NAME = " << this->getName()
-                  << "}, {EFFECT = " << this->getEffect() << "} !!" << std::endl;
+                  << "}, {EFFECT = " << this->getEffect() << "} , {Spell Type -> ";
+        printSpellType(this->type);
+        std::cout << "} !!" << std::endl;
     }
     virtual SpellCard *clone() const override
     {
@@ -198,10 +231,48 @@ public:
     virtual void play() const override
     {
         std::cout << "[TRAP CARD] ~~> {NAME = " << this->getName()
-                  << "}, {EFFECT = " << this->getEffect() << "} !!" << std::endl;
+                  << "}, {EFFECT = " << this->getEffect() << "} {Trap type -> ";
+        printTrapType(this->type);
+        std::cout << "} !!" << std::endl;
     }
     virtual TrapCard *clone() const
     {
         return new TrapCard(*this);
+    }
+};
+
+class PendulumCard : public SpellCard, public MonsterCard
+{
+private:
+    int pendulumScale;
+
+    bool isValidPenScale(const int scale)
+    {
+        return (scale > 0 && scale < 13);
+    }
+
+public:
+    // Def constructor
+    PendulumCard() : Card(), MonsterCard(), SpellCard(), pendulumScale(1) {}
+    // Par constructor
+    PendulumCard(const int penScale, const int penAttack, const int penDeff, const char *penName, const char *penEffect) : Card(penName, penEffect), MonsterCard(penAttack, penDeff, penName, penEffect), SpellCard(SpellType::Continuous, penName, penEffect)
+    {
+        if (isValidPenScale(penScale))
+            this->pendulumScale = penScale;
+        // else ~PendulumCard();
+    }
+    // Copy constructor -> no need because of no dyn memory
+    // Operator = -> no need because of no dyn memory
+    // Destructor -> no need becase of no dyn memory
+    virtual PendulumCard *clone() const override
+    {
+        return new PendulumCard(*this);
+    }
+    virtual void play() const override
+    {
+        std::cout << "[PENDULUM CARD] ~~> {NAME = " << this->getName()
+                  << "}, {EFFECT = " << this->getEffect() << "}, "
+                  << "{ATTACK = " << this->getAttack() << ", DEFFENSE = " << this->getDefense()
+                  << "}!!" << std::endl;
     }
 };
