9f7035a6efe687db560a077845e093e8b5bd6b4f
KKSotirov <sotirov.k73@gmail.com>
2026-07-26 09:14:13 +0300

    Added base class of LOL.cpp

---
+++ Summer vibecoding/LOL.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <cstring>
+#include <stdexcept>
+#include <utility>
+
+class Entity
+{
+private:
+    char *name;
+    const int id;
+    static int counter;
+
+    void free()
+    {
+        delete[] this->name;
+    }
+
+protected:
+    void setterHelper(char *dest, const char *src)
+    {
+        char *temp = src ? new char[strlen(src) + 1] : nullptr;
+        if (src != nullptr)
+            strcpy(temp, src);
+        delete[] dest;
+        dest = temp;
+    }
+
+    void swap(Entity &other) noexcept
+    {
+        std::swap(this->name, other.name);
+        // id's cannot be copied
+    }
+
+public:
+    // Dyn Mem ==> RO3
+    // Def constr
+    Entity() : name(nullptr), id(++counter) {}
+    // Par constr
+    Entity(const char *newName) : name(nullptr), id(++counter)
+    {
+        setterHelper(this->name, newName);
+    }
+    // Copy constructor
+    Entity(const Entity &other) : id(++counter)
+    {
+        setterHelper(this->name, other.name);
+    }
+    // Operator =
+    Entity &operator=(Entity other)
+    {
+        this->swap(other);
+        return *this;
+    }
+    // Destructor
+    ~Entity()
+    {
+        free();
+    }
+
+    // Operator >
+    bool operator>(const Entity &other)
+    {
+        return this->id > other.id;
+    }
+    bool operator<(const Entity &other)
+    {
+        return !(this->id > other.id);
+    }
+    void operator<<(const Entity &object)
+    {
+        std::cout << "[KEY INFO] ~> {ID = " << object.id
+                  << "},  {NAME = " << object.name << " }"
+                  << std::endl;
+    }
+    virtual void operator+(const Entity *other) = 0;
+    virtual Entity *operator*(const int multiplier) const = 0;
+    virtual Entity *clone() const = 0;
+    virtual Entity *activate() const = 0; // use ability
+    virtual Entity &operator++() = 0;     // Level up
+};
+
+int Entity::counter = 0;
