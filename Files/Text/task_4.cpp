12d70a3f94b3cfcc6fdf441713bec31f332f4b4f
KKSotirov <sotirov.k73@gmail.com>
2026-08-31 21:58:20 +0300

    Finished task with fstreams

--- Files/Text/task_4.cpp
+++ Files/Text/task_4.cpp
@@ -4,12 +4,13 @@
 
 struct Song
 {
-    char *title;
+    char *title = nullptr;
     int durationSec;
     double rating;
 
     void init(const char *newTitle, const int newDuration, const double newRating)
     {
+        delete[] title;
         title = new char[strlen(newTitle) + 1];
         strcpy(title, newTitle);
         durationSec = newDuration;
@@ -59,3 +60,79 @@ struct Song
         return !in.fail();
     }
 };
+
+void resize(Song *&songs, size_t &capacity, const size_t size)
+{
+    capacity *= 2;
+    try
+    {
+        Song *tmp = new Song[capacity];
+        for (size_t i = 0; i < size; i++)
+        {
+            strcpy(tmp[i].title, songs[i].title);
+            tmp[i].durationSec = songs[i].durationSec;
+            tmp[i].rating = songs[i].rating;
+
+            songs[i].destroy();
+        }
+        songs = tmp;
+    }
+    catch (...)
+    {
+        throw;
+    }
+}
+
+int main()
+{
+    // >> Song* songs
+    // std::cout << longestword
+    // free mem
+
+    std::ifstream in("playlist.txt");
+    if (!in)
+    {
+        std::cerr << "Error opening the file! \n";
+        return 1;
+    }
+
+    size_t capacity = 2;
+    size_t size = 0;
+    Song *songs = new Song[capacity];
+    Song currentSong;
+
+    while (currentSong.readFromFile(in))
+    {
+
+        if (size == capacity)
+        {
+            resize(songs, capacity, size);
+        }
+        songs[size++] = currentSong;
+        currentSong.title = nullptr;
+    }
+
+    in.close();
+    if (size == 0)
+    {
+        std::cout << "The playlist is empty! \n";
+        delete[] songs;
+        return 0;
+    }
+
+    int posMax = 0;
+    for (size_t i = 0; i < size; i++)
+    {
+        if (songs[i].durationSec > songs[posMax].durationSec)
+            posMax = i;
+    }
+
+    std::cout << "Longest song is: " << songs[posMax].title << "\n";
+
+    for (size_t i = 0; i < size; i++)
+    {
+        songs->destroy();
+    }
+
+    return 0;
+}
