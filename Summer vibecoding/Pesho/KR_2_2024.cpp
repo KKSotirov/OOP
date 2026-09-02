021a018be937919988aabd451a658f3badea79e5
KKSotirov <sotirov.k73@gmail.com>
2026-09-02 22:40:06 +0300

    Added start of task 2 from Pesho prep

---
+++ Summer vibecoding/Pesho/KR_2_2024.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+
+enum class WeaponType
+{
+    Sword,
+    Wand,
+    Staff,
+    Axe
+};
+
+struct Position
+{
+    int x;
+    int y;
+
+    bool isAdjacentTo(const Position &other) const
+    {
+        int dx = abs(x - other.x);
+        int dy = abs(y - other.y);
+        return (dx == 1) && (dy == 1);
+    }
+};
+
+class Player
+{
+private:
+    char *name;
+    WeaponType weapon;
+    Position pos;
+    unsigned ad;
+    unsigned hp;
+
+    void free()
+    {
+        delete[] name;
+        name = nullptr;
+    }
+
+protected:
+    void setterHelper(char *&dest, const char *src)
+    {
+        char *tmp = src ? new char[strlen(src) + 1] : nullptr;
+        if (src)
+            strcpy(tmp, src);
+        delete[] dest;
+        dest = tmp;
+    }
+
+public:
+    // RO3
+    Player(const char *_name, const WeaponType _weapon, const Position _pos, const unsigned _ad, const unsigned _hp) : name(nullptr), weapon(_weapon), pos(_pos), ad(_ad), hp(_hp)
+    {
+        setName(_name);
+    }
+
+    Player(const Player &other) : name(nullptr), weapon(other.weapon), pos(other.pos), ad(other.ad), hp(other.hp)
+    {
+        setName(other.name);
+    }
+
+    Player operator=(const Player &other)
+    {
+        if (this != &other)
+        {
+            setName(other.name);
+        }
+        return *this;
+    }
+
+    virtual ~Player()
+    {
+        free();
+    }
+
+    // GETTERS + SETTERS
+
+    const char *getName() const
+    {
+        return name;
+    }
+
+    void setName(const char *_name)
+    {
+        setterHelper(name, _name);
+    }
+
+    // move, handle attack, attack, get attack power, print
+    bool move(const int targetX, const int targetY, const int moves)
+    {
+        if (pos.x == targetX && pos.y == targetY)
+        {
+            std::cout << "Successfully reached position : [" << targetX << "," << targetY << "]  after " << moves << " moves. \n";
+            return true;
+        }
+
+        if (pos.x < targetX)
+            pos.x++;
+        else if (pos.x > targetX)
+            pos.x--;
+
+        if (pos.y < targetY)
+            pos.y++;
+        else if (pos.y > targetY)
+            pos.y--;
+
+        return move(targetX, targetY, moves + 1);
+    }
+};
